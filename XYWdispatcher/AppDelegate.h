c065302bf319692aec60a7641d96db20ab91bb21
xueyongwei <xueyongwei@eoemarket.com>
2016-11-14 15:32:10 +0800

    Initial Commit

---
+++ XYWdispatcher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYWdispatcher
+//
+//  Created by xueyongwei on 16/11/14.
+//  Copyright © 2016年 xueyongwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XYWdispatcher/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XYWdispatcher
+//
+//  Created by xueyongwei on 16/11/14.
+//  Copyright © 2016年 xueyongwei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
